0b8dfc0dea1cba16c02193b6aa3da02fccf2dce1
Ryder252 <shanebo2017@gmail.com>
2026-03-24 13:32:26 -0400

    Initial commit for Selenium Tests.

--- obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7638a253fffeccbbc2e4a02c112b4e2c2ba3433d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01183485d2a58b6e22df0e998b7bcf040ee7abad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
