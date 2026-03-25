7aa9eaa4c9716c6f8d82b64ec37e7633e9044c6a
Ryder252 <shanebo2017@gmail.com>
2026-03-24 23:21:57 -0400

    BaseTest.cs

--- obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8dfc0dea1cba16c02193b6aa3da02fccf2dce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43200b7df4b20a3777124ab7527e473008d6eaf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
