6984c64f91f7c47c0270a12478fdceddb87fb2d3
Ryder252 <shanebo2017@gmail.com>
2026-03-24 23:37:01 -0400

    First BaseTest edits.

---
+++ SeleniumTests/BaseTest.cs
@@ -0,0 +1,59 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System;
+
+namespace SeleniumTests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        private const string BaseUrl = "http://127.0.0.1:5500/MyFitnessBud_Frontend/"; // Adjust to your application URL
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            var options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl(BaseUrl);
+        }
+
+        [TearDown]
+        public virtual void Teardown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+        }
+
+        protected void WaitForElement(By locator, int timeoutSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.Until(ExpectedConditions.PresenceOfElementLocated(locator));
+        }
+
+        protected IWebElement FindElement(By locator)
+        {
+            return driver.FindElement(locator);
+        }
+
+        protected void Click(By locator)
+        {
+            WaitForElement(locator);
+            FindElement(locator).Click();
+        }
+
+        protected void SendKeys(By locator, string text)
+        {
+            WaitForElement(locator);
+            FindElement(locator).SendKeys(text);
+        }
+
+        protected string GetText(By locator)
+        {
+            WaitForElement(locator);
+            return FindElement(locator).Text;
+        }
+    }
+}
--- obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43200b7df4b20a3777124ab7527e473008d6eaf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa9eaa4c9716c6f8d82b64ec37e7633e9044c6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
