c53a50ea7e36140e5b4df52a27b1a948c851abfb
Ryder252 <shanebo2017@gmail.com>
2026-04-13 22:30:01 -0400

    Move files

--- obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8224ce28a43a0d68aeb7f9f95158f9098c054d54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39074192ac213479049079b5811663f277764fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
