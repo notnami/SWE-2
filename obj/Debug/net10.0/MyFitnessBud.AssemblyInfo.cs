7f2d34a3620d94fd9846af28616f76e868d88742
Ryder252 <shanebo2017@gmail.com>
2026-04-16 14:05:47 -0400

    Dockerfile

--- obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFitnessBud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadc3462091bb4853267d566343cb0ae8464ba90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adda6a19268b3877d0e908000b41b5e4ae5e6e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFitnessBud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
